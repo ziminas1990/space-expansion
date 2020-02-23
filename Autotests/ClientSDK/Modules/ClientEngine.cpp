bbe8e050915f9881491f3fb3950edab56bc53fe5
ziminas1990 <ziminas1990@gmail.com>
2020-02-23 14:47:57 +0300

    SES-59 IEngine interface has been reworked and simplified

--- Autotests/ClientSDK/Modules/ClientEngine.cpp
+++ Autotests/ClientSDK/Modules/ClientEngine.cpp
@@ -5,7 +5,7 @@ namespace autotests { namespace client {
 bool Engine::getSpecification(EngineSpecification& specification)
 {
   spex::Message request;
-  request.mutable_engine()->mutable_getspecification();
+  request.mutable_engine()->set_specification_req(true);
   if (!send(request))
     return false;
 
@@ -15,14 +15,14 @@ bool Engine::getSpecification(EngineSpecification& specification)
   if (response.choice_case() != spex::IEngine::kSpecification)
     return false;
 
-  specification.nMaxThrust = response.specification().maxthrust();
+  specification.nMaxThrust = response.specification().max_thrust();
   return true;
 }
 
 bool Engine::setThrust(geometry::Vector thrust, uint32_t nDurationMs)
 {
   spex::Message request;
-  spex::IEngine::SetThrust *pBody = request.mutable_engine()->mutable_setthrust();
+  spex::IEngine::SetThrust *pBody = request.mutable_engine()->mutable_change_thrust();
   pBody->set_x(thrust.getX());
   pBody->set_y(thrust.getY());
   pBody->set_thrust(uint32_t(thrust.getLength()));
@@ -33,17 +33,17 @@ bool Engine::setThrust(geometry::Vector thrust, uint32_t nDurationMs)
 bool Engine::getThrust(geometry::Vector &thrust)
 {
   spex::Message request;
-  request.mutable_engine()->mutable_getthrust();
+  request.mutable_engine()->set_thrust_req(true);
   if (!send(request))
     return false;
 
   spex::IEngine response;
   if (!wait(response))
     return false;
-  if (response.choice_case() != spex::IEngine::kCurrentThrust)
+  if (response.choice_case() != spex::IEngine::kThrust)
     return false;
 
-  spex::IEngine::CurrentThrust const& currentThrust = response.currentthrust();
+  spex::IEngine::CurrentThrust const& currentThrust = response.thrust();
   thrust.setPosition(currentThrust.x(), currentThrust.y());
   thrust.setLength(currentThrust.thrust());
   return true;
