7b71a4ca9dd047513609b57a5d6277406d36656c
ziminas1990 <ziminas1990@gmail.com>
2020-07-15 22:45:27 +0300

    SES-86 A 'Server' class has been implemented

---
+++ python-sdk/server/server.py
@@ -0,0 +1,29 @@
+from typing import Optional
+import subprocess
+import tempfile
+import sys
+import yaml
+
+from server.configurator.configuration import Configuration
+
+
+class Server:
+    """Class provides functionality to run and manage a space expansion server.
+    Note that a path to the 'space-expansion-server' executable must be listed
+    in the 'PATH' environment variable
+    """
+
+    def __init__(self):
+        self.server_process: Optional[subprocess.Popen] = None
+        self.config_file = tempfile.NamedTemporaryFile(mode="w")
+
+    def run(self, configuration: Configuration):
+        self.config_file.write(yaml.dump(configuration.to_pod()))
+        self.config_file.flush()
+
+        self.server_process = subprocess.Popen(
+                ["space-expansion-server", self.config_file.name],
+                stdout=sys.stdout)
+
+    def stop(self):
+        self.server_process.kill()
