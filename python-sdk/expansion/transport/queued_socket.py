1a19e60a9043e377fd6d8e195c742f0946cfa814
ziminas1990 <ziminas1990@gmail.com>
2020-10-10 18:42:40 +0300

    Fix building issue: add QueuedSocket to project

---
+++ python-sdk/expansion/transport/queued_socket.py
@@ -0,0 +1,13 @@
+from .queued_terminal import QueuedTerminal
+
+
+class QueuedSocket(QueuedTerminal):
+    """What the hell? Why QueuedSocket is a QueuedTerminal?
+    Because their interfaces and behavior (and, subsequently, implementation)
+    are the same. So, what is the different?
+    Well, the 'QueuedTerminal' should better be used as a base class. But the
+    'QueuedSocket' should be used as standalone object, when inheriting
+    'QueuedTerminal' makes no sense. In other words, this class has been
+    introduced to make client's code more understandable.
+    """
+    pass
