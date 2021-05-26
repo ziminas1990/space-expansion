9b1fbeb846eccf559fc091ae904d23aa1ac1d16a
ziminas1990 <ziminas1990@gmail.com>
2021-05-27 00:21:50 +0300

    Fix Newton engine error

--- server/Newton/NewtonEngine.cpp
+++ server/Newton/NewtonEngine.cpp
@@ -14,27 +14,28 @@ bool NewtonEngine::prephare(uint16_t, uint32_t, uint64_t)
 
 void NewtonEngine::proceed(uint16_t, uint32_t nIntervalUs, uint64_t)
 {
-  const uint32_t step = 256;
+  const uint32_t step = 64;
   double nIntervalSec = nIntervalUs / 1000000.0;
 
-  uint32_t nId = m_nNextObjectId.fetch_add(step);
-  uint32_t end = std::min(nId + step, PhysicalObject::TotalInstancies());
-  while (nId < end) {
-    PhysicalObject* pObject = PhysicalObject::Instance(nId);
-    if (pObject) {
-      // acc_t - acceleration * time
-      geometry::Vector acc_t;
-      for (geometry::Vector const& externalForce : pObject->m_externalForces)
-        acc_t += externalForce;
-      acc_t *= nIntervalSec/pObject->m_weight;
+  uint32_t begin = m_nNextObjectId.fetch_add(step);
+  while (begin < PhysicalObject::TotalInstancies()) {
+    uint32_t end = std::min(begin + step, PhysicalObject::TotalInstancies());
+    for (uint32_t nId = begin; nId < end; ++nId) {
+      PhysicalObject* pObject = PhysicalObject::Instance(nId);
+      if (pObject) {
+        // acc_t - acceleration * time
+        geometry::Vector acc_t;
+        for (geometry::Vector const& externalForce : pObject->m_externalForces)
+          acc_t += externalForce;
+        acc_t *= nIntervalSec/pObject->m_weight;
 
-      geometry::Vector movement(pObject->m_velocity, nIntervalSec);
-      movement.add(acc_t, nIntervalSec * 0.5);
-      pObject->m_position.translate(movement);
-      pObject->m_velocity += acc_t;
+        geometry::Vector movement(pObject->m_velocity, nIntervalSec);
+        movement.add(acc_t, nIntervalSec * 0.5);
+        pObject->m_position.translate(movement);
+        pObject->m_velocity += acc_t;
+      }
     }
-    nId = m_nNextObjectId.fetch_add(step);
-    end = std::min(nId + step, PhysicalObject::TotalInstancies());
+    begin = m_nNextObjectId.fetch_add(step);
   }
 }
 
