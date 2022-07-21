354da9a46e56bad0ed29ded47908714d6cb03362
Masha <mashery-77@mail.ru>
2022-07-21 18:18:54 +0300

    first commit

--- web-client/static/ItemsContainer.js
+++ web-client/static/ItemsContainer.js
@@ -1,4 +1,4 @@
-
+let last_items = new Map;
 class Position {
   constructor([x, y, vx, vy]) {
     this.x = x;
@@ -6,6 +6,7 @@ class Position {
     this.vx = vx;
     this.vy = vy;
   }
+
 }
 
 class Item {
@@ -17,15 +18,28 @@ class Item {
     this.timestamp = timestamp;
   }
 
+   update() {
+      //считаем ускорение
+      let key_update_item = this.strkey();
+      let last_item = items.items.get(key_update_item); //id: 0 item_type: "asteroid" position: Position  ax: 0 ay: 0 vx: 1 vy: 1 x: 7169.459495998263 y: 7269.459495736856 [[Prototype]]: Object radius: 50 timestamp: 7297158952
+      this.position.ax = (this.position.vx - last_item.position.vx)/(this.timestamp - last_item.timestamp);
+      this.position.ay = (this.position.vy - last_item.position.vy)/(this.timestamp - last_item.timestamp);
+      console.log(this)
+      //отправляем новый переписанный item в мапу itemsContainer
+      items.update_item(this);
+      console.log(items);
+   }
+
+
   strkey() {
     return this.item_type + "." + this.id;
   }
 
   predict_position(at) {
     let dt_sec = (at - this.timestamp) / 10**6
-    return {
-      "x": this.position.x + this.position.vx * dt_sec,
-      "y": this.position.y + this.position.vy * dt_sec
+    return { 
+      "x": this.position.x + (this.position.vx * dt_sec + this.position.ax * dt_sec),
+      "y": this.position.y + (this.position.vy * dt_sec + this.position.ay * dt_sec),
     }
   }
 };
@@ -34,6 +48,7 @@ class ItemsContainer {
   constructor() {
     this.items = new Map();
   }
+
 
   update_item(item) {
     this.items.set(item.strkey(), item)
@@ -50,5 +65,4 @@ class ItemsContainer {
     }
     return items_stream;
   }
-
 };
