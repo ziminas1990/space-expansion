5bc08adf2dfb0f29003c15f9ecc770c3438aa61a
Masha <mashery-77@mail.ru>
2022-07-21 18:18:54 +0300

    first commit

--- web-client/static/main.js
+++ web-client/static/main.js
@@ -1,18 +1,17 @@
-
 function set_status(status) {
   document.getElementById("status").innerHTML = status
 }
 
 function runApp() {
   timestamp = {
-    "server_us": 0,
-    "local_ms": 0
+   "server_us": 0,
+   "local_ms": 0
   }
 
   predict_now = function () {
     return timestamp.server_us + (Date.now() - timestamp.local_ms) * 1000
   }
-
+  
   // Create components:
   connection = new Connection()
   items = new ItemsContainer()
@@ -31,19 +30,26 @@ function runApp() {
       document.getElementById("status").innerHTML = "Connection established";
     }
   )
-  connection.addEventListener('message', function (event) {
-    const update = JSON.parse(event.data);
-    timestamp.server_us = update.ts
-    timestamp.local_ms = Date.now()
-    set_status(JSON.stringify(timestamp));
-    for (let object_info of update.items) {
-      item = new Item(object_info)
-      item.timestamp = timestamp.server_us
-      items.update_item(item)
-    }
-  });
+   let item = undefined;
+   connection.addEventListener('message', function (event) { 
+   const update = JSON.parse(event.data);  
+   timestamp.server_us = update.ts;             
+   timestamp.local_ms = Date.now()               
+   set_status(JSON.stringify(timestamp)); 
+   if(item === undefined) {                
+      for (let object_info of update.items) {
+         item = new Item(object_info);          
+         item.timestamp = timestamp.server_us;  
+         items.update_item(item);
+   }} 
+   else {                                                              
+      for (let object_info of update.items) {   
+         //?console.log("hi from messege")
+         let update_item = new Item(object_info);
+         update_item.update();
+      }}               
+   });
 
-  // Start scene animation
   animation = new Konva.Animation(function (frame) {
     scene.update(items, predict_now())
   });
