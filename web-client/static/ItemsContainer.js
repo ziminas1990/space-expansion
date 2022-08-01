099a115f164afefa87cd5841910f3e6f938d5675
Aleksandr Zimin <aleksandr.s.zimin@jettycloud.com>
2022-08-02 00:20:49 +0400

    Fix items prediction problem
    + minor codestyle fixes

--- web-client/static/ItemsContainer.js
+++ web-client/static/ItemsContainer.js
@@ -11,25 +11,26 @@ class Position {
 }
 
 class Item {
-   constructor({type: item_type, id, pos: position, radius=10, ts: timestamp = undefined}) {
+  constructor({type: item_type, id, pos: position, radius=10, ts: timestamp = undefined}) {
     this.item_type = item_type;
     this.id        = id;
     this.position  = new Position(position);
     this.radius    = radius;
     this.timestamp = timestamp;
-   }
+  }
 
-   update(item_from_update) {                
+  update(item_from_update) {
     this.position.ax = (item_from_update.pos[2] - this.position.vx)/(item_from_update.ts - this.timestamp); 
     this.position.ay = (item_from_update.pos[3] - this.position.vy)/(item_from_update.ts - this.timestamp);
     this.timestamp = item_from_update.ts;   
-   }
+  }
 
-   strkey() {
+  strkey() {
     return this.item_type + "." + this.id;
-   }
+  }
 
-   predict_position(at) {
+  predict_position(at) {
+    let dt_sec = (at - this.timestamp) / 10**6
     let dt_half_sqr = dt_sec * dt_sec / 2;
     return { 
       "x": this.position.x + (this.position.vx * dt_sec + this.position.ax * dt_half_sqr),
@@ -41,18 +42,18 @@ class Item {
 class ItemsContainer {
   constructor() {
     this.items = new Map();
-   }
+  }
 
-   update_item(item) {                             
-    let str_key = item.type + "." + item.id;       
-      if (this.items.has(str_key)) {              
-         let last_item = this.items.get(str_key); 
-         last_item.update(item);                  
-      } else {                                    
-         let new_item = new Item(item);           
-         this.items.set(str_key, new_item);                              
-      }
-   }  
+  update_item(item) {
+    let str_key = item.type + "." + item.id;
+    if (this.items.has(str_key)) {
+       let last_item = this.items.get(str_key);
+       last_item.update(item);
+    } else {
+       let new_item = new Item(item);
+       this.items.set(str_key, new_item);
+    }
+  }
 
   items_stream() {
     return this.items.values()
